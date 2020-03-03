789c7f9ddc9006d1dc95641dcbb3e402ec27c582
Cédric Ernewein <25933633+Cernewein@users.noreply.github.com>
2020-03-03 15:19:14 +0100

    Visualization of DDPG results

--- vars.py
+++ vars.py
@@ -1,11 +1,11 @@
 import torch
 ### General settings
 TIME_STEP_SIZE = 10*60# How many seconds are in one of our timeteps? For example if we want every minute, set this to 60
-NUM_HOURS = 31*24
+NUM_HOURS = 24
 NUM_TIME_STEPS = int(NUM_HOURS*3600//TIME_STEP_SIZE) # A total of 12 hours computed every second
 
 ##### RL Agent parameters
-NUM_EPISODES = 1 # Number of episodes
+NUM_EPISODES = 4000 # Number of episodes
 EPSILON = 1 # For epsilon-greedy approach
 EPS_DECAY = 0.9998
 LEARNING_RATE = 0.0001
@@ -13,7 +13,7 @@ LEARNING_RATE_ACTOR = 1e-4
 LEARNING_RATE_CRITIC = 1e-3
 GAMMA = 0.99
 TARGET_UPDATE = 10
-BATCH_SIZE = 32
+BATCH_SIZE = 16
 N_ACTIONS = 2
 INPUT_DIMS = 5
 FC_1_DIMS = 300
@@ -24,7 +24,7 @@ TAU = 0.001 # For soft update
 MEMORY_SIZE = 45000 # For storing approx 100 episodes worth of data
 
 ##### Environment parameters
-COMFORT_PENALTY = 0.1 # Penalty applied when going outside of "comfort" bounds
+COMFORT_PENALTY = 10 # Penalty applied when going outside of "comfort" bounds
 T_MIN = 19.5 # Minimum temperature that should be achieved inside of the building
 T_MAX = 22.5 # Maximum temperature that should be achieved inside of the building
 C_I = 2.07*3.6e6 # Based on Emil Larsen's paper - heat capacity of the building
